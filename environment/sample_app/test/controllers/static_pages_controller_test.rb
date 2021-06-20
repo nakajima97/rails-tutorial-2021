1c68e49f134a45ae54d41593baa80b6fac408964
Yudai Nakajima <kituneudon97@gmail.com>
2021-06-20 06:35:36 +0000

    Chapter4を終えた

--- environment/sample_app/test/controllers/static_pages_controller_test.rb
+++ environment/sample_app/test/controllers/static_pages_controller_test.rb
@@ -10,7 +10,7 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     get static_pages_home_url
     # HTTPステータスコード200が返ってくることを検証
     assert_response :success
-    assert_select "title", "Home | #{@base_title}"
+    assert_select "title", "#{@base_title}"
   end
 
   test "should get help" do
