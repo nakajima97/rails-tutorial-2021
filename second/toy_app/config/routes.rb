708957c3f01d93ed4757510c4704d37e3dc6bd76
Yudai Nakajima <kituneudon97@gmail.com>
2021-08-12 07:06:17 +0000

    finish chapter 2

--- second/toy_app/config/routes.rb
+++ second/toy_app/config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :microposts
+  resources :users
+  # 作成されたURL
+  # GET /users
+  # GET /users/1
+  # GET /users/new
+  # POST /users
+  # GET /users/1/edit
+  # PATCH /users/1
+  # DELETE /users/1
+  root 'users#index'
 end
