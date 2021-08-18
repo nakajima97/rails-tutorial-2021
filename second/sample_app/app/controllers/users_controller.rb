7a33b59d28a37fbe1103eecf694e6dd39476e574
Yudai Nakajima <kituneudon97@gmail.com>
2021-08-18 08:02:16 +0000

    Finish chapter 11

--- second/sample_app/app/controllers/users_controller.rb
+++ second/sample_app/app/controllers/users_controller.rb
@@ -17,9 +17,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      log_in @user
+      @user.send_activation_email
       flash[:success] = 'Welcome to the Sample App!'
-      redirect_to @user
+      redirect_to root_url
     else
       render 'new'
     end
